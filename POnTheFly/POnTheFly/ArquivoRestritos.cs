1fce19dd347ed64844bf88030ea31c2e1a3dd8ce
Giovani Rocha Lima <Watzeckdev@gmail.com>
2022-09-13 12:00:02 -0300

    Adicionado Classes bloqueados e restritos, já com os métodos]

---
+++ POnTheFly/POnTheFly/ArquivoRestritos.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POnTheFly
+{
+    public class ArquivoRestritos
+    {
+        public string CPF { get; set; }
+
+        public ArquivoRestritos Proximo { get; set; }
+        public List<ArquivoRestritos> Filhos { get; set; }
+        public ArquivoRestritos(string CPF)
+        {
+
+            this.CPF = CPF;
+
+            this.Proximo = null;
+        }
+
+        public override string ToString()
+        {
+            return "\nDados Restritos:\nCPF:" + CPF;
+        }
+        public void GravarArquivoRestritos(List<ArquivoRestritos> arquivodeRestritos)
+        {
+            Console.WriteLine("Iniciando a Gravação de Dados...");
+            try
+            {
+                StreamWriter sw = new StreamWriter(@"C:\Users\WATZECK\Desktop\PONTHEFLY\POnTheFly\Restritos.dat");  //Instancia um Objeto StreamWriter (Classe de Manipulação de Arquivos)
+                                                                                                                            //sw.WriteLine("Treinamento de C#");  //Escreve uma linha no Arquivo
+                                                                                                                            //sw.WriteLine("maria;araraquara;190;contato;"); //Exemplo de escrita - formato da escrita será de acordo com a necessidade do projeto
+                foreach (ArquivoRestritos i in arquivodeRestritos)
+                {
+                    sw.WriteLine(i.CPF);
+                }
+                sw.Close();  // Comando para Fechar o Arquivo
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executando o Bloco de Comandos.");
+            }
+            Console.WriteLine("FIM DA GRAVAÇÃO");
+            Console.ReadKey();
+
+        }
+        public void CarregarArquivoRestritos(List<ArquivoRestritos> arquivodeRestritos)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"C:\Users\WATZECK\Desktop\PONTHEFLY\POnTheFly\Bloqueados.dat"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        //tempo = new DateTime(int.Parse(line.Substring(14, 4)), int.Parse(line.Substring(12, 2)), int.Parse(line.Substring(10, 2)), int.Parse(line.Substring(20, 2)), int.Parse(line.Substring(18, 2)), int.Parse(line.Substring(18, 2)));
+                        arquivodeRestritos.Add(new ArquivoRestritos
+                            (
+                            line.Substring(0, 11)
+                            ));
+                    }
+
+                    Console.WriteLine("\nArquivo carregado com sucesso!");
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+        public static string ReadCPF(string text)
+        {
+            string cpfString;
+            long cpfLong;
+            int digVerificador, v1, v2, aux;
+            int[] digitosCPF = new int[9];
+            bool digitosIguais = false;
+
+            do
+            {
+                Console.Write(text);
+                cpfString = Console.ReadLine();
+                while (!long.TryParse(cpfString, out cpfLong))
+                {
+                    Console.Write("Digite um CPF valido!\n{0}", text);
+                    cpfString = Console.ReadLine();
+                }
+                digVerificador = (int)(cpfLong % 100);
+                cpfLong /= 100;
+                for (int i = 0; i < 9; i++)
+                {
+                    aux = (int)cpfLong % 10;
+                    digitosCPF[i] = aux;
+                    cpfLong /= 10;
+                }
+                digitosIguais = false;
+                for (int i = 0; i < digitosCPF.Length; i++)
+                {
+                    if (i == digitosCPF.Length - 1)
+                    {
+                        Console.WriteLine("O CPF nao segue as regras de validacao da Receita Federal!");
+                        digitosIguais = true;
+                        break;
+                    }
+                    if (digitosCPF[i] != digitosCPF[i + 1]) break;
+                }
+                if (digitosIguais) continue;
+                v1 = v2 = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    v1 += digitosCPF[i] * (9 - i);
+                    v2 += digitosCPF[i] * (8 - i);
+                }
+                v1 = (v1 % 11) % 10;
+                v2 += v1 * 9;
+                v2 = (v2 % 11) % 10;
+                if (v1 * 10 + v2 == digVerificador) return cpfString;
+                else Console.WriteLine("O CPF nao segue as regras de validacao da Receita Federal!");
+            } while (true);
+        }
+
+    }
+}
---
+++ POnTheFly/POnTheFly/ListaRestritos.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POnTheFly
+{
+    public class ListaRestritos
+    {
+        public ArquivoRestritos HEAD { get; set; }
+        public ArquivoRestritos TAIL { get; set; }
+
+        public ListaRestritos()
+        {
+            HEAD = TAIL = null;
+        }
+
+        private bool Vazia()
+        {
+            if ((HEAD == null) && (TAIL == null))
+                return true;
+            else
+                return false;
+        }
+
+        public void Print()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("LISTA VAZIA!");
+            }
+            else
+            {
+                ArquivoRestritos aux = HEAD;
+                Console.WriteLine("Lista de CPFs restritos: ");
+                do
+                {
+                    Console.WriteLine(aux.ToString() + "\n");
+                    aux = aux.Proximo;
+                } while (aux != null);
+                Console.WriteLine("\nFIM DA Lista");
+            }
+            Console.ReadKey();
+        }
+
+        public void Push(ArquivoRestritos aux)
+        {
+
+
+            //INSERÇÃO LISTA VAZIA
+            if (Vazia())
+            {
+                this.HEAD = this.TAIL = aux;
+            }
+            else
+            {
+
+                if (aux.CPF.CompareTo(TAIL.CPF) >= 0)
+                {
+                    TAIL.Proximo = aux;
+                    TAIL = aux;
+                }
+                else
+                {
+                    if (aux.CPF.CompareTo(HEAD.CPF) < 0)
+                    {
+                        aux.Proximo = HEAD;
+                        HEAD = aux;
+                    }
+                    else
+                    {
+                        ArquivoRestritos aux1, aux2;
+                        aux1 = this.HEAD;
+                        aux2 = this.HEAD;
+
+                        do
+                        {
+                            if (aux.CPF.CompareTo(aux1.CPF) >= 0)
+                            {
+                                aux2 = aux1;
+                                aux1 = aux1.Proximo;
+                            }
+                            else
+                            {
+
+                                aux2.Proximo = aux;
+                                aux.Proximo = aux1;
+
+                                break;
+                            }
+                        } while (true);
+
+
+                    }
+                }
+            }
+        }
+
+        //removendo
+        public void pop(string CPFRemovido)
+        {
+            if (Vazia())
+                Console.WriteLine("Lista Vazia! Impossivel remover.");
+            else
+            {
+                HEAD = HEAD.Proximo;
+                Console.WriteLine("CPF [" + CPFRemovido + "] removido!");
+            }
+            Console.WriteLine("\nAperte [enter] para continuar.");
+            Console.ReadKey();
+        }
+
+
+        //Localizando
+        public void Find(string CPF)
+        {
+            if (Vazia())
+                Console.WriteLine("Lista Vazia!");
+            else
+            {
+                ArquivoRestritos auxiliar = HEAD;
+                bool achou = false;
+                do
+                {
+                    if (auxiliar.CPF == CPF)
+                    {
+                        Console.WriteLine("CPF localizado!\n");
+                        Console.WriteLine(auxiliar.ToString());
+                        Console.WriteLine("\n");
+                        achou = true;
+                    }
+                    auxiliar = auxiliar.Proximo;
+                } while (auxiliar != null);
+                if (!achou)
+                    Console.WriteLine("CPF [" + CPF + "] não existente.\n");
+                else
+                    Console.WriteLine("\nFim da lista.");
+
+                Console.WriteLine("\nAperte [enter] para continuar.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
